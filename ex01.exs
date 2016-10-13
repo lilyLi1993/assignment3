dfc699dfe523f00f9fdc18d0d7a7ab0be07261e8
Li Li <lli2@smu.edu>
2016-10-12 21:12:10 -0500

    my_assign3

--- ex01.exs
+++ ex01.exs
@@ -1,34 +1,25 @@
 
 defmodule Ex01 do
 
-  @moduledoc """
-
-  In this exercise you'll implement a simple process that acts as a
-  counter. Each time you send it `{:next, from}`, it will send to
-  `from` the tuple `{:next_is, value}`, and that value will increase
-  by one on each call. You code will allow you to set the initial
-  value returned.
-
-  The tests in the following module are commented out.
-
-  You'll uncomment each in turn, and then write the code in the
-  Ex01 module to make that test pass. You've finished when all the
-  tests are uncommented and pass.
-
-  Do NOT use the Task or Agent libraries for this test.
-
-  ------------------------------------------------------------------
-  ## For each test (2 in all):  10
-
-        6 does the code compile and pass the tests
-        2 is the program written in an idiomatic style that uses
-          appropriate and effective language and library features
-        2 is the program well laid out,  appropriately using indentation,
-          blank lines, vertical alignment
-  """
   
   def counter(value \\ 0) do
+    receive do
+      {:next, from} ->
+      send from, {:next_is, value}
+      counter(value + 1)
+    end
   end
+  
+  def new_counter(value)do
+    spawn(Ex01, :counter, [value])
+  end
+   
+  def next_value(counter)do
+    send counter, {:next, self}
+    receive do
+      {:next_is, value} -> value
+    end
+  end 
 
 end
 
@@ -41,35 +32,29 @@ defmodule Test do
   # This test assumes you have a function `counter` that can be spawned
   # and which handles the `{:next, from}` message
 
-  # test "basic message interface" do
-  #   count = spawn Ex01, :counter, []
-  #   send count, { :next, self }
-  #   receive do
-  #     { :next_is, value } ->
-  #       assert value == 0
-  #   end
-  # 
-  #   send count, { :next, self }
-  #   receive do
-  #     { :next_is, value } ->
-  #       assert value == 1
-  #   end
-  # end
+  test "basic message interface" do
+    count = spawn Ex01, :counter, []
+    send count, { :next, self }
+    receive do
+      { :next_is, value } ->
+        assert value == 0
+    end
+  
+    send count, { :next, self }
+    receive do
+      { :next_is, value } ->
+        assert value == 1
+    end
+  end
 
   # then uncomment this one
   # Now we add two new functions to Ex01 that wrap the use of
   # that counter function, making the overall API cleaner
 
-  # test "higher level API interface" do
-  #   count = Ex01.new_counter(5)
-  #   assert  Ex01.next_value(count) == 5
-  #   assert  Ex01.next_value(count) == 6
-  # end
+  test "higher level API interface" do
+    count = Ex01.new_counter(5)
+    assert  Ex01.next_value(count) == 5
+    assert  Ex01.next_value(count) == 6
+  end
 
 end
-
-
-
-
-
-
--- ex02.exs
+++ ex02.exs
@@ -1,6 +1,23 @@
 
 defmodule Ex02 do
 
+  def new_counter(value\\0) do
+    {:ok, counter} = Agent.start_link(fn -> value end)
+     counter
+   end
+ 
+   def next_value(counter) do
+     Agent.get_and_update(counter, &{&1, (&1 + 1)})
+   end
+ 
+   def new_global_counter( value \\ 0 ) do
+     Agent.start_link(fn -> value end, name: Ex02)
+   end
+ 
+   def global_next_value do
+     Agent.get_and_update(Ex02, &{&1, (&1 + 1)})
+   end
+
 end
 
 ExUnit.start()
@@ -9,14 +26,10 @@ defmodule Test do
   use ExUnit.Case
 
   @moduledoc """
-
   In this exercise you'll use agents to implement the counter.
-
   You'll do this three times, in three different ways.
-
   ------------------------------------------------------------------
   ## For each test (3 in all):  10
-
         6 does the code compile and pass the tests
         2 is the program written in an idiomatic style that uses
           appropriate and effective language and library features
@@ -28,30 +41,29 @@ defmodule Test do
   @doc """
   First uncomment this test. Here you will be inserting code
   to create and access the agent inline, in the test itself.
-
   Replace the placeholders with your code.
   """
 
-  # test "counter using an agent" do
-  #   { :ok, counter } = « your code »
-  # 
-  #   value   = « your code »
-  #   assert value == 0
-  # 
-  #   value   = « your code »
-  #   assert value == 1
-  # end
+   test "counter using an agent" do
+     { :ok, counter } = Agent.start_link( fn -> 0 end )
+   
+     value   = Agent.get_and_update(counter, &{&1, (&1 + 1)})
+     assert value == 0
+   
+     value   = Agent.get_and_update(counter, &{&1, (&1 + 1)})
+     assert value == 1
+   end
 
   @doc """
   Next, uncomment this test, and add code to the Ex02 module at the
   top of this file to make those tests run.
   """
 
-  # test "higher level API interface" do
-  #   count = Ex02.new_counter(5)
-  #   assert  Ex02.next_value(count) == 5
-  #   assert  Ex02.next_value(count) == 6
-  # end
+   test "higher level API interface" do
+     count = Ex02.new_counter(5)
+     assert  Ex02.next_value(count) == 5
+     assert  Ex02.next_value(count) == 6
+   end
 
   @doc """
   Last (for this exercise), we'll create a global counter by adding
@@ -60,16 +72,10 @@ defmodule Test do
   that agent into calls to `global_next_value`?
   """
 
-  # test "global counter" do
-  #   Ex02.new_global_counter
-  #   assert Ex02.global_next_value == 0
-  #   assert Ex02.global_next_value == 1
-  #   assert Ex02.global_next_value == 2
-  # end
+   test "global counter" do
+     Ex02.new_global_counter
+     assert Ex02.global_next_value == 0
+     assert Ex02.global_next_value == 1
+     assert Ex02.global_next_value == 2
+   end
 end
-
-
-
-
-
-
